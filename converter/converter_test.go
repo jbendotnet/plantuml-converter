4b8478d8798d0620cfed84b80da110204effea3e
karlderkaefer <karlderkaefer@users.noreply.github.com>
2020-07-14 11:59:08 +0200

    add test for set files

--- converter/converter_test.go
+++ converter/converter_test.go
@@ -1,19 +1,43 @@
 package converter
 
 import (
-	"encoding/hex"
 	"fmt"
 	"github.com/signavio/plantuml-converter/cmd"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
 func Test_GenerateLink(t *testing.T) {
 	input := "Bob -> Alice : hello"
 	output := GenerateLink(input)
-	decoded, err := hex.DecodeString(output)
-	assert.NoError(t, err)
-	assert.Equal(t, input, decoded)
 	assert.Contains(t, output, cmd.PlantUmlServer)
 	fmt.Println(output)
+}
+
+func Test_SetFiles(t *testing.T) {
+	uml := PlantUml{}
+	files := []string{"bla.txt", "some.html", "readme.md", "other-readme.md"}
+	expected := []PlantUmlFile{
+		{
+			filePath: "readme.md",
+			blocks:   nil,
+		},
+		{
+			filePath: "other-readme.md",
+			blocks:   nil,
+		},
+	}
+	for _, file := range files {
+		writeEmptyFile(file)
+		defer os.Remove(file)
+	}
+	uml.SetFiles()
+	assert.Equal(t, expected, uml.files)
+}
+
+func writeEmptyFile(filename string) {
+	data := []byte("")
+	ioutil.WriteFile(filename, data, os.ModePerm)
 }
