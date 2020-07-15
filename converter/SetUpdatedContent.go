3371fded50fccc1d583c0394384f860ef81c2545
mikud <michael.kudla@signavio.com>
2020-07-15 16:48:52 +0200

    Fix problem with SetUpdatedContent.go if file ends with @enduml
    * Correct loop over the file blocks and add the markdown link at the end of the file
    * Adjust test file to cover this fix in testBlockSucceededSingle.md
    * Move test from SetBlocks_test to SetUpdatedContent_test.go

--- converter/SetUpdatedContent.go
+++ converter/SetUpdatedContent.go
@@ -20,19 +20,18 @@ func (f *PlantUmlFile) SetUpdatedContent() {
 	// you can always update the markdown file because the hash will be the same
 	// if the content does not change
 	lines := strings.Split(f.fileContent, "\n")
-	for i := 0; i < len(lines); i++ {
+	for i := 0; i <= len(lines); i++ {
 		isLineOfBlock, block := IsLineNUmberAnEndOfAPlantUmlBlock(f, i)
 		if isLineOfBlock {
 			// insert the markdown
 			f.updatedContent = f.updatedContent + "![](" + block.markdownLink + ")\n"
 
-			if strings.HasPrefix(lines[i], "![]("+PlantUmlServerUrl) == false {
+			if i < len(lines) && strings.HasPrefix(lines[i], "![]("+PlantUmlServerUrl) == false {
 				f.updatedContent = f.updatedContent + lines[i] + "\n"
 			}
 		} else {
 			f.updatedContent = f.updatedContent + lines[i] + "\n"
 		}
-
 	}
 
 }
